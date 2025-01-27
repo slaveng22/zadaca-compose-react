3d22ffeaaf9258db0c3cb547b728a11b87828f9e
slaveng22 <slaven.gugolj@outlook.com>
2025-01-27 15:45:54 +0100

    fixed compose typos and updated Readme

--- builddockerimages.sh
+++ builddockerimages.sh
@@ -3,8 +3,8 @@ docker build -t slaveng/messageboard-front:react .
 docker push slaveng/messageboard-front:react
 
 cd ../back
-docker build -t slaveng/messageboard-api:testing:react .
-docker push slaveng/messageboard-api:testing:react
+docker build -t slaveng/messageboard-api:react .
+docker push slaveng/messageboard-api:react
 
 cd ../../votingapp/front
 docker build -t slaveng/votingapp-front:react .
